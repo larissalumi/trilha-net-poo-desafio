564f1f48e31463bcaf9a9592ff8784b766728d0b
Larissa Lumi <larissalumih@gmail.com>
2024-10-24 01:01:12 -0300

    TO DO'S feitos

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("No Iphone:");
+Smartphone i1 = new Iphone(numero: "11966666666", modelo: "XR", imei: "1111111111", memoria: 64);
+
+i1.InstalarAplicativo("Whatsapp");
+i1.Ligar();
+Console.WriteLine("\n");
+
+Console.WriteLine("No Nokia:");
+Smartphone n1 = new Nokia(numero: "11968686866", modelo: "Modelo 1", imei: "212121121", memoria: 128);
+
+i1.InstalarAplicativo("Whatsapp Business");
+n1.ReceberLigacao();
+
+
+
